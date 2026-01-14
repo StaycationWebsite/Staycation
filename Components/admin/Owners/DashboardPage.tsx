55d44e0f8d7a126e0c1a2f1311cd22e17cc2476a
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-01-14 11:05:25 +0800

    Fix lint error

--- Components/admin/Owners/DashboardPage.tsx
+++ Components/admin/Owners/DashboardPage.tsx
@@ -71,7 +71,7 @@ interface DashboardPageProps {
   onPaymentClick: () => void;
   onBookingClick: () => void;
   onPoliciesClick: () => void;
-  onDateClick: (date: Date, havenName: string) => void;
+  onDateClick: (date: Date, haven: Haven) => void;
   havens: Haven[];
 }
 
@@ -200,7 +200,7 @@ useEffect(() => {
       day.date
     );
     if (selectedHaven) {
-      onDateClick(clickedDate, selectedHaven.haven_name || selectedHaven.name || '');
+      onDateClick(clickedDate, selectedHaven);
     }
   };
 
